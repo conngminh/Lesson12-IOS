53ae865a631eedcc46c0496447c8927ed420f04a
conngminh <congminhhttt@gmail.com>
2022-02-10 16:40:12 +0700

    ADWebImage library

--- ViewController.swift
+++ ViewController.swift
@@ -7,14 +7,33 @@
 
 import UIKit
 import Toast_Swift
+import SDWebImage
 
 class ViewController: UIViewController {
     @IBOutlet weak var tfUsername: UITextField!
     @IBOutlet weak var tfPass: UITextField!
+    @IBOutlet weak var imgLogo: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupImageLogo()
     }
+    
+    // Code sử dụng SDwebimage
+    func setupImageLogo() {
+//        imgLogo.backgroundColor = .gray
+//        if let url = URL(string: "https://www.pesmaster.com/pes-2021-mobile/graphics/players/149953879_l.png") {
+//            let data = try! Data(contentsOf: url)
+//            imgLogo.image = UIImage(data: data)
+//        }
+        
+        if let url = URL(string: "https://www.pesmaster.com/pes-2021-mobile/graphics/players/149953879_l.png") {
+            self.imgLogo.sd_setImage(with: url, placeholderImage: UIImage(named: "vn"))
+
+        }
+    }
+    
+    //
     
     @IBAction func tapOnLogin(_ sender: Any) {
         view.endEditing(true)
